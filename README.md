bf170d2ac6f8dffb5483e6deeb77c8a84aadffd7
Fred Perkins <Fred.Perkins@derivco.co.uk>
2020-07-15 09:47:44 +0100

    Updated readme and adding comment about debugger error

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # PluginTest
-A series of plugin tests to load external assemblies into a WASM application.
+This repository is a demo of how you can load assemblies remotely and load them into the current app domain.
+The model is designed so that it could be ran server side, or on a standard CLR environment - Where `IAssemblyLoadContext` would need to be implemented per platform.
--- StandalonePlugin/StandalonePlugin.cs
+++ StandalonePlugin/StandalonePlugin.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using PluginHost;
 
 namespace StandalonePlugin
@@ -12,6 +13,18 @@ namespace StandalonePlugin
 
         public void Initialise()
         {
+            //Running Debugger.Break(); throws the following exception in WASM
+            //dotnet.3.2.0.js:1 Uncaught (in promise) RuntimeError: abort(undefined). Build with -s ASSERTIONS=1 for more info.
+            //    at abort (https://localhost:5001/_framework/wasm/dotnet.3.2.0.js:1:16061)
+            //    at _abort (https://localhost:5001/_framework/wasm/dotnet.3.2.0.js:1:112430)
+            //    at stub_debugger_agent_user_break (<anonymous>:wasm-function[4142]:0xb7d23)
+            //    at do_debugger_tramp (<anonymous>:wasm-function[1897]:0x50b9f)
+            //    at interp_exec_method (<anonymous>:wasm-function[1120]:0x250f6)
+            //    at interp_runtime_invoke (<anonymous>:wasm-function[5655]:0xf7391)
+            //    at mono_jit_runtime_invoke (<anonymous>:wasm-function[5109]:0xddb3d)
+            //    at do_runtime_invoke (<anonymous>:wasm-function[1410]:0x3ba85)
+            //    at mono_runtime_try_invoke (<anonymous>:wasm-function[418]:0xcfdb)
+            //    at mono_runtime_invoke (<anonymous>:wasm-function[1610]:0x44b39)
             Console.WriteLine($"[{nameof(StandalonePlugin)}]{nameof(Initialise)}");
         }
     }
